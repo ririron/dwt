ace96d71ca346491c76ffc317133209b4aca57ec
ririron <townageriron@gmail.com>
2020-06-16 17:34:31 +0900

    scaling and wavelet Coef

--- waveletCoef.py
+++ waveletCoef.py
@@ -1,39 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scale import calcScalingCoef
 
-def calcScalingCoef(f):
+
+
+def calcWaveletCoef(f):
     y = []
     i = 0
-    p0 = p1 = np.sqrt(2)**-1
+    q0 = np.sqrt(2) ** -1
+    q1 = -1 * q0
     for x in f:
         if i > len(f) -2:
             break
-        ansR = p0 * f[i]
-        ansL = p1 * f[i+1]
+        ansR = q0 * f[i]
+        ansL = q1 * f[i+1]
         result = ansR + ansL
         i += 2
         y.append(result)
 
     return y
 
-'''
-
 t = np.linspace(-5, 5, 16) 
 f = np.sin(t)
 
 sk0 = f #skの初期値
-
 sk1 = calcScalingCoef(f)
-sk2 = calcScalingCoef(sk1)
+wk1 = calcWaveletCoef(sk0)
+
 
 plt.subplot(3, 1, 1)
 plt.scatter(t, sk0)
 
 plt.subplot(3, 1, 2)
-plt.scatter(t[0:-1:2], sk1)
+plt.scatter(t[::2], sk1, color='red')
 
 plt.subplot(3, 1, 3)
-plt.scatter(t[0:-1:4], sk2)
+plt.scatter(t[::2], wk1, color='red')
+
 
 plt.show()
-'''
