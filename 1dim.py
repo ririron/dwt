3e93848e1262bcbe5cf4545bd66abc116495d102
ririron <townageriron@gmail.com>
2020-06-17 14:02:39 +0900

    tiny modify in 1dim

--- 1dim.py
+++ 1dim.py
@@ -13,19 +13,19 @@ sk2 = calcScalingCoef(sk1)
 wk1 = calcWaveletCoef(sk0)
 wk2 = calcWaveletCoef(sk1)
 
-plt.subplot(5, 1, 1)
+plt.subplots_adjust(wspace=0.4, hspace=0.6)
+
+plt.subplot(3, 1, 1)
+plt.title("Sk0 = f[n]")
 plt.scatter(t, sk0)
 
-plt.subplot(5, 1, 2)
+plt.subplot(3, 1, 2)
+plt.title("Sk1")
 plt.scatter(t[::2], sk1, color='red')
 
-plt.subplot(5, 1, 3)
+plt.subplot(3, 1, 3)
+plt.title("Wk1")
 plt.scatter(t[::2], wk1, color='red')
 
-plt.subplot(5, 1, 4)
-plt.scatter(t[::4], sk2, color='green')
-
-plt.subplot(5, 1, 5)
-plt.scatter(t[::4], wk2, color='green')
 
 plt.show()
